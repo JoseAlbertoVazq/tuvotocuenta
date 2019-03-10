bf68affdeb3800b0d3a0472204ecfc874440238b
JoseAlbertoVazq <danielguillen10>
2019-03-10 16:20:17 +0100

    .

--- api/src/api/user/controller.js
+++ api/src/api/user/controller.js
@@ -13,6 +13,7 @@ export const index = ({ querymen: { query, select, cursor } }, res, next) =>
     .then(success(res))
     .catch(next)
 
+    
 export const show = ({ params }, res, next) =>
   User.findById(params.id)
     .then(notFound(res))
