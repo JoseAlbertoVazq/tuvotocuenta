04c5529da453b916078cc085aaaaa4210b48e26b
JoseAlbertoVazq <danielguillen10>
2019-03-11 17:18:54 +0100

    borra propuestas, falta arreglar editar propuesta

--- web/starter/src/app/dialogs/propuestas/dialog-delete-propuesta/dialog-delete-propuesta.component.ts
+++ web/starter/src/app/dialogs/propuestas/dialog-delete-propuesta/dialog-delete-propuesta.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { PropuestaService } from '../../../services/propuesta.service';
 
 @Component({
   selector: 'app-dialog-delete-propuesta',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog-delete-propuesta.component.scss']
 })
 export class DialogDeletePropuestaComponent implements OnInit {
+  checkedRobot: boolean;
+  constructor(private propuestaService: PropuestaService, public snackBar: MatSnackBar,
+    public dialogRef: MatDialogRef<DialogDeletePropuestaComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
-  constructor() { }
 
   ngOnInit() {
+  }
+  captcha() {
+    if (this.checkedRobot) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  delete() {
+    this.propuestaService.deletePropuesta(this.data.id).subscribe(resp => {
+      this.dialogRef.close('confirm');
+    }, error => this.snackBar.open('Hubo un error al intentar borrar esta propuesta', 'Close', {duration: 3000}));
   }
 
 }
--- web/starter/src/app/dialogs/propuestas/dialog-edit-propuesta/dialog-edit-propuesta.component.ts
+++ web/starter/src/app/dialogs/propuestas/dialog-edit-propuesta/dialog-edit-propuesta.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Propuesta } from '../../../interfaces/propuesta-response';
+import { PropuestaContainer } from '../../../interfaces/propuesta-container';
+import { PropuestaService } from '../../../services/propuesta.service';
+import { PartidoService } from 'src/app/services/partido.service';
+import { MateriaService } from 'src/app/services/materia.service';
+import { PropuestaCreateDto } from 'src/app/dto/propuesta-create-dto';
+import { Partido } from 'src/app/interfaces/partido-response';
+import { Materia } from 'src/app/interfaces/materias-response';
+
 
 @Component({
   selector: 'app-dialog-edit-propuesta',
@@ -6,10 +17,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog-edit-propuesta.component.scss']
 })
 export class DialogEditPropuestaComponent implements OnInit {
+  public form: FormGroup;
+  allPropuestas: Propuesta[];
+  propuesta: Propuesta;
+  materiaSelected;
+  partidoSelected;
+  allPartidos: Partido[];
+  allMaterias: Materia[];
+  constructor(private fb: FormBuilder, private partidoService: PartidoService, private propuestaService: PropuestaService,
+    private materiaService: MateriaService,
+    public dialogRef: MatDialogRef<DialogEditPropuestaComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
-  constructor() { }
+   ngOnInit() {
+     this.propuesta = this.data.propuesta;
+     this.getAllMaterias();
+     this.getAllPartidos();
+     this.form = this.fb.group({
+      titulo: [this.data.propuesta.titulo, Validators.compose([Validators.required])],
+      contenido: [this.data.propuesta.contenido, Validators.compose([Validators.required])],
+      partido: [this.data.propuesta.partido.nombre, Validators.compose([Validators.required])],
+      materia: [this.data.propuesta.materia.nombre, Validators.compose([Validators.required])]
+    });
+   }
 
-  ngOnInit() {
-  }
+   editPropuesta() {
+     const propuestaCreateDto: PropuestaCreateDto = <PropuestaCreateDto>this.form.value;
+     this.propuestaService.updatePropuesta(this.propuesta.id, propuestaCreateDto).subscribe(
+       propuesta => {
+         this.dialogRef.close();
+       }
+     );
+   }
+
+   getPropuestas() {
+     this.propuestaService.getAllPropuestas().subscribe(r => this.allPropuestas = r.rows);
+   }
+
+   getAllPartidos() {
+     this.partidoService.getAllPartidos().subscribe(r => this.allPartidos = r.rows);
+   }
+
+   getAllMaterias() {
+     this.materiaService.getAllMaterias().subscribe(r => this.allMaterias = r.rows);
+   }
 
 }
